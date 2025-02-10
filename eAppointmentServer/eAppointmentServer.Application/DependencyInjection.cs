f0a245d7159344edc372d1eaa0ef72e719de9c54
Emre ÖZGÖZ <emreozgoz@gmail.com>
2025-02-10 13:10:33 +0300

    init application

---
+++ eAppointmentServer/eAppointmentServer.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAppointmentServer.Application
+{
+    public class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(IServiceCollection services)
+        {
+            services.AddMediatR(conf =>
+            {
+                conf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+            return services;
+        }
+    }
+}
---
+++ eAppointmentServer/eAppointmentServer.Application/Features/Auth/Login/LoginCommandResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAppointmentServer.Application.Features.Auth.Login
+{
+    public sealed record LoginCommandResponse(
+    string Token);
+}
